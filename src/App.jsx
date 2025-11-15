e5671be6f9d74501473ce88d13f049d1c839b7df
DariushOps <overlord127001@gmail.com>
2025-11-15 18:41:34 +0330

    settings done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./pages/RootLayout";
+import ErrorPage from "./pages/ErrorPage";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import About from "./pages/About";
+import ContactUs from "./pages/ContactUs";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "shop",
+        element: <Shop />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contactUs",
+        element: <ContactUs />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <h1 className="ml-4 mt-4">DariushShop</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
