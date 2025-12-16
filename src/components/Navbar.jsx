a32517ad4a7706cf7945b86375a25aa140181f88
DariushOps <overlord127001@gmail.com>
2025-12-16 21:22:21 +0330

    fully responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import SidebarCart from "./SidebarCart";
 import { motion, AnimatePresence } from "framer-motion";
+import { SiX } from "react-icons/si";
 
 function CartIcon({ count, onClick }) {
   return (
@@ -22,6 +23,7 @@ function CartIcon({ count, onClick }) {
 export default function Navbar() {
   const [fixed, setFixed] = useState(false);
   const [showCart, setShowCart] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
   const total = useSelector((state) => state.cart.totalQuantities);
 
   function handelShowCart() {
@@ -49,7 +51,7 @@ export default function Navbar() {
     <>
       <nav
         className={`
-    fixed top-0 left-0 right-0 z-100 flex justify-between items-center uppercase font-montserrat text-[1.9rem] px-12
+    fixed top-0 left-0 right-0 z-100 flex justify-between items-center uppercase font-montserrat sm:text-[1.2rem] md:text-[1.5rem] lg:text-[1.9rem] lg:px-12 sm:px-3 px-6 
     transition-all duration-500 ease-in-out
     ${
       fixed
@@ -59,12 +61,12 @@ export default function Navbar() {
   `}
       >
         <div className="flex items-center justify-center pl-2">
-          <Link className="text-[2.8rem] font-semibold text-amber-500/90 hover:text-amber-400 transition-colors duration-500 font-greatVibes capitalize text-center">
+          <Link className="text-[2.5rem] sm:text-[2rem] lg:text-[2.8rem] font-semibold text-amber-500/90 hover:text-amber-400 transition-colors duration-500 font-greatVibes capitalize text-center">
             DOW
           </Link>
         </div>
 
-        <div className="flex items-center justify-center gap-16 ml-40">
+        <div className=" items-center justify-center gap-5 sm:ml-20 lg:gap-12 lg:ml-32 hidden sm:flex">
           <NavLink className={linkClasses} to="/">
             Home
           </NavLink>
@@ -79,7 +81,7 @@ export default function Navbar() {
           </NavLink>
         </div>
 
-        <div className="flex items-center justify-center gap-5 text-[1.4rem]">
+        <div className="hidden sm:flex items-center justify-center gap-2 lg:gap-5 sm:text-lg md:text-xl lg:text-[1.4rem]">
           <CartIcon count={total} onClick={handelShowCart} />
           <Link className="hover:text-amber-400 duration-500" to="login">
             Log In
@@ -88,7 +90,54 @@ export default function Navbar() {
             Sign Up
           </Link>
         </div>
+        <div className="sm:hidden flex justify-center items-start -mt-4">
+          <button
+            className="flex flex-col gap-1"
+            onClick={() => setShowMenu(true)}
+          >
+            <span className="block w-10 h-1 bg-gray-200"></span>
+            <span className="block w-10 h-1 bg-gray-200"></span>
+            <span className="block w-10 h-1 bg-gray-200"></span>
+          </button>
+        </div>
       </nav>
+      <AnimatePresence mode="popLayout">
+        {showMenu && (
+          <div className="relative">
+            <motion.div
+              key="menu"
+              initial={{ x: "100%" }}
+              animate={{ x: 0 }}
+              exit={{ x: "100%" }}
+              layout
+              transition={{
+                type: "spring",
+                stiffness: 250,
+                damping: 50,
+              }}
+              className="fixed w-full flex-col gap-10 text-4xl font-montserrat uppercase h-screen flex items-center justify-center bg-slate-900 z-100"
+            >
+              <NavLink className={linkClasses} to="/">
+                Home
+              </NavLink>
+              <NavLink className={linkClasses} to="shop">
+                Shop
+              </NavLink>
+              <NavLink className={linkClasses} to="about">
+                About
+              </NavLink>
+              <NavLink className={linkClasses} to="contactUs">
+                Contact Us
+              </NavLink>
+              <div className="absolute flex top-12 right-12 text-3xl">
+                <button onClick={() => setShowMenu(false)}>
+                  <SiX />{" "}
+                </button>
+              </div>
+            </motion.div>
+          </div>
+        )}
+      </AnimatePresence>
       <AnimatePresence mode="popLayout">
         {showCart && <SidebarCart onClick={handelShowCart} />}
       </AnimatePresence>
